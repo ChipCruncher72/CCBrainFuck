4f206c31363a34f835303f7d1703ed712b466995
ChipCruncher <waityouguysareliving@gmail.com>
2024-03-19 15:47:48 +1030

    Added better io streaming and info for the REPL

--- src/main-repl.cpp
+++ src/main-repl.cpp
@@ -5,21 +5,25 @@
 #include "proj_functions.cpp"
 
 int main() {
-    fmt::println("Brainfuck REPL v{}.{}.{} by ChipCruncher72 (https://www.github.com/ChipCruncher72)\n",
+    fmt::println("Brainfuck REPL v{}.{}.{} by ChipCruncher72 (https://www.github.com/ChipCruncher72)",
         CCBF_MAJOR,
         CCBF_MINOR,
         CCBF_PATCH
     );
+    fmt::println("INFO: Type \"~EOF\" to exit the REPL\n");
 
     std::array<char, 30000> tape;
     int pointer = 0;
 
     std::string inp;
+    fmt::print(">>> ");
     std::getline(std::cin, inp);
 
     while (inp != "~EOF") {
         interpret(inp, tape, pointer);
+        std::cin.ignore();
         fmt::println("");
+        fmt::print(">>> ");
         std::getline(std::cin, inp);
     }
 
